fea717f5ba17d42053f3f8ec2fd175bbe5a09d5a
mbasitmukhtar <basitmukhtar210@gnail.com>
2020-12-11 15:08:20 +0500

    Initial commit

---
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.git_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        android.widget.Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        
+    }
+}

c61d0f2d5be8d10a0dab6fee92b9ba498f946b69
Muhammad Basit <basitmukhtar210@gmail.com>
2020-12-11 15:32:03 +0500

    second commit

--- app/src/main/java/com/example/git_test/MainActivity.java
+++ app/src/main/java/com/example/git_test/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.git_test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +12,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        android.widget.Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "1", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2", Toast.LENGTH_SHORT).show();
         
     }
 }
